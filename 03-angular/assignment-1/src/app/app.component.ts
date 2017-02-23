48510bf98548e9fc97581244ed2128ef9638cb9d
Student 6 <student6@ITR405Mac6.local>
2017-02-23 17:54:09 -0600

    3.1

--- 03-angular/assignment-1/src/app/app.component.ts
+++ 03-angular/assignment-1/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
     table  { border-collapse: collapse; }
     td, th { border: 1px solid black; padding: 3px 8px; }
     th     { text-align: left; }
+    td.no-border { border: 0px }
   </style>
   <p><b>Student: </b>Bob Smith</p>
   <p><b>Email: </b>bobsmith@gmail.com</p>
@@ -48,12 +49,15 @@ import { Component } from '@angular/core';
       <th>Points Scored</th>
       <th>Points Possible</th>
       <th>Percent</th>
+      <th>Grade</th>
     </tr>
-    <tr *ngFor="let assignment of AssignmentList">
+    <tr *ngFor="let assignment of AssignmentList; let num = index">
       <td>{{ assignment.assignment}}</td>
       <td>{{ assignment.scored }}</td>
       <td>{{ assignment.possible }}</td>
       <td>{{ assignment.percent | percent }}</td>
+      <td>{{ assignment.grade }}</td>
+      <td class= 'no-border'><button (click)='deleteAssignment(num)'>x</button></td>
     </tr>
   </table>
   `,
@@ -86,21 +90,36 @@ export class AppComponent {
       scored: this.ScoredPoints,
       possible: this.PointsPossible,
       percent: this.ScoredPoints / this.PointsPossible,
+      grade: this.calcGrade(this.ScoredPoints / this.PointsPossible,),
     }
 
     this.TotalScoredPoints += this.ScoredPoints;
     this.TotalPointsPossible += this.PointsPossible;
     this.TotalPercentGrade = this.TotalScoredPoints / this.TotalPointsPossible;
-    this.Grade = this.calcGrade();
+    this.Grade = this.calcGrade(this.TotalPercentGrade);
 
     this.AssignmentList.push(newAssignment);
   }
 
-  calcGrade():string{
-    if (this.TotalPercentGrade >= .9) { return "A" }
-    if (this.TotalPercentGrade >= .8) { return "B" }
-    if (this.TotalPercentGrade >= .7) { return "C" }
-    if (this.TotalPercentGrade >= .6) { return "D" }
+  deleteAssignment(num:number):void {
+    this.TotalScoredPoints -= this.AssignmentList[num].scored;
+    this.TotalPointsPossible -= this.AssignmentList[num].possible;
+    this.TotalPercentGrade = this.TotalScoredPoints / this.TotalPointsPossible;
+    this.Grade = this.calcGrade(this.TotalPercentGrade);
+
+    this.AssignmentList.splice(num, 1);
+
+    if (this.AssignmentList.length === 0){
+      this.TotalPercentGrade = 0;
+      this.Grade = "N/A";
+    }
+  }
+
+  calcGrade(num:number):string{
+    if (num >= .9) { return "A" }
+    if (num >= .8) { return "B" }
+    if (num >= .7) { return "C" }
+    if (num >= .6) { return "D" }
     return "F";
   }
 }
@@ -110,4 +129,5 @@ interface assignment {
   scored: number;
   possible: number;
   percent: number;
+  grade: string;
 }
